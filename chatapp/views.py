fac23446ee29d04fed358ffc6584835845bae5cf
yashwant162 <yashwantsingh162@gmail.com>
2023-07-19 16:07:08 +0530

    urls and views added for room page

--- chatapp/views.py
+++ chatapp/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 import threading
+
+
+
 def index(request):
     print(threading.get_native_id())
     return render(request, "chatapp/index.html")
 
 # Create your views here.
+def room(request, room_name):
+    return render(request, "chatapp/room.html", {"room_name": room_name})

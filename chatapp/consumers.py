fcb8cbb91b52efec70142b97ca45a9c2bd6cccf3
yashwant162 <yashwantsingh162@gmail.com>
2023-07-21 13:09:26 +0530

    prettier kara bs

--- chatapp/consumers.py
+++ chatapp/consumers.py
@@ -24,7 +24,11 @@ class ChatConsumer(AsyncWebsocketConsumer):
 
         # Send message to room group
         await self.channel_layer.group_send(
-            self.room_group_name, {"type": "chat_message", "message": message}
+            
+            self.room_group_name, {
+                "type": "chat_message",
+                "message": message
+            }
         )
 
     # Receive message from room group
--- chatapp/views.py
+++ chatapp/views.py
@@ -4,7 +4,7 @@ import threading
 
 
 def index(request):
-    print(threading.get_native_id())
+    # print(threading.get_native_id())
     return render(request, "chatapp/index.html")
 
 # Create your views here.
